3c4f1819276acf8fb39c6c02ca90b4169c8e51c0
Tian Shi <david881223@outlook.com>
2024-04-06 13:55:32 +0000

    modify config file read path

--- bedrock/config.py
+++ bedrock/config.py
@@ -1,5 +1,9 @@
+import os
 import yaml
 
-with open("bedrock/config.yml", "r") as file:
+file_dir = os.path.dirname(os.path.abspath(__file__))
+config_file = os.path.join(file_dir, "config.yml")
+
+with open(config_file, "r") as file:
     config = yaml.safe_load(file)
 
