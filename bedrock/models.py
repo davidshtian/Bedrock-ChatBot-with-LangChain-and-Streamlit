8037222bd7799692b9555f8d7c493835a083f651
davidshtian <david881223@outlook.com>
2024-08-28 11:29:56 +0800

    Update for Converse API

--- bedrock/models.py
+++ bedrock/models.py
@@ -9,4 +9,7 @@ class ChatModel:
         self.model_config = config["models"][model_name]
         self.model_id = self.model_config["model_id"]
         self.model_kwargs = model_kwargs
-        self.llm = ChatBedrockConverse(model_id=self.model_id, max_tokens=self.model_kwargs["max_tokens"], temperature=self.model_kwargs["temperature"], top_p=self.model_kwargs["top_p"], additional_model_request_fields={"top_k": self.model_kwargs["top_k"]})
+        if "mistral" in self.model_id:
+            self.llm = ChatBedrockConverse(model=self.model_id, max_tokens=self.model_kwargs["max_tokens"], temperature=self.model_kwargs["temperature"], top_p=self.model_kwargs["top_p"])
+        else:
+            self.llm = ChatBedrockConverse(model=self.model_id, max_tokens=self.model_kwargs["max_tokens"], temperature=self.model_kwargs["temperature"], top_p=self.model_kwargs["top_p"], additional_model_request_fields={"top_k": self.model_kwargs["top_k"]})
